1e17aec0cacada70c9360a930f05e1b4a93ecfd1
Dustin Lee <dustinsl094@gmail.com>
2023-02-10 11:45:54 -0500

    Node.js Express Routing Exercise - solution

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import config from "./config";
 // TODO: import router from routes/
-
+import apiRouter from "./routes";
 const app = express();
 
 app.use(express.json());
 
 // TODO: use the imported router to handle all requests
+app.use("/api", apiRouter);
 
 app.use((err, req, res, next) => {
   console.error(err);
